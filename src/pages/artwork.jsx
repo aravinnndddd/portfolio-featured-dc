510d886bd1055782705232c44ffb89060cac3e33
aravind <aromalaravind0@gmial.com>
2025-03-01 01:56:48 +0530

    edited artworks :)

--- src/pages/artwork.jsx
+++ src/pages/artwork.jsx
@@ -7,37 +7,37 @@ export const Artwork = () => {
        <h5><center>art-works</center></h5>
       </div>
       <section className="works">
-        <img className='artworks' src="./src/assets/img29.jpg" alt="" />
-        <img className='artworks' src="./src/assets/img1.png" alt="" />
-        <img className='artworks' src="./src/assets/img2.png" alt="" />
-        <img className='artworks' src="./src/assets/img3.png" alt="" />
-        <img className='artworks' src="./src/assets/img4.png" alt="" />
-        <img className='artworks' src="./src/assets/img5.png" alt="" />
-        <img className='artworks' src="./src/assets/img6.png" alt="" />
-        <img className='artworks' src="./src/assets/img7.png" alt="" />
-        <img className='artworks' src="./src/assets/img8.jpg" alt="" />
-        <img className='artworks' src="./src/assets/img9.jpg" alt="" />
-        <img className='artworks' src="./src/assets/img10.jpeg" alt="" />
-        <img className='artworks' src="./src/assets/img11.jpeg" alt="" />
-        <img className='artworks' src="./src/assets/img12.jpeg" alt="" />
-        <img className='artworks' src="./src/assets/img13.jpeg" alt="" />
-        <img className='artworks' src="./src/assets/img14.jpg" alt="" />
-        <img className='artworks' src="./src/assets/img15.jpg" alt="" />
-        <img className='artworks' src="./src/assets/img16.jpg" alt="" />
-        <img className='artworks' src="./src/assets/img17.jpg" alt="" />
-        <img className='artworks' src="./src/assets/img18.jpg" alt="" />
-        <img className='artworks' src="./src/assets/img19.jpg" alt="" />
-        <img className='artworks' src="./src/assets/img20.jpg" alt="" />
-        <img className='artworks' src="./src/assets/img21.jpg" alt="" />
-        <img className='artworks' src="./src/assets/img22.jpg" alt="" />
-        <img className='artworks' src="./src/assets/img23.jpg" alt="" />
-        <img className='artworks' src="./src/assets/img24.jpg" alt="" />
-        <img className='artworks' src="./src/assets/img25.jpg" alt="" />
-        <img className='artworks' src="./src/assets/img26.jpg" alt="" />
-        <img className='artworks' src="./src/assets/img27.jpg" alt="" />
-        <img className='artworks' src="./src/assets/img28.jpg" alt="" />
-        <img className='artworks' src="./src/assets/img30.jpg" alt="" />
-        <img className='artworks' src="./src/assets/img31.jpg" alt="" />
+        <img className='artworks' src="/assets/img29.jpg" alt="" />
+        <img className='artworks' src="/assets/img1.png" alt="" />
+        <img className='artworks' src="/assets/img2.png" alt="" />
+        <img className='artworks' src="/assets/img3.png" alt="" />
+        <img className='artworks' src="/assets/img4.png" alt="" />
+        <img className='artworks' src="/assets/img5.png" alt="" />
+        <img className='artworks' src="/assets/img6.png" alt="" />
+        <img className='artworks' src="/assets/img7.png" alt="" />
+        <img className='artworks' src="/assets/img8.jpg" alt="" />
+        <img className='artworks' src="/assets/img9.jpg" alt="" />
+        <img className='artworks' src="/assets/img10.jpeg" alt="" />
+        <img className='artworks' src="/assets/img11.jpeg" alt="" />
+        <img className='artworks' src="/assets/img12.jpeg" alt="" />
+        <img className='artworks' src="/assets/img13.jpeg" alt="" />
+        <img className='artworks' src="/assets/img14.jpg" alt="" />
+        <img className='artworks' src="/assets/img15.jpg" alt="" />
+        <img className='artworks' src="/assets/img16.jpg" alt="" />
+        <img className='artworks' src="/assets/img17.jpg" alt="" />
+        <img className='artworks' src="/assets/img18.jpg" alt="" />
+        <img className='artworks' src="/assets/img19.jpg" alt="" />
+        <img className='artworks' src="/assets/img20.jpg" alt="" />
+        <img className='artworks' src="/assets/img21.jpg" alt="" />
+        <img className='artworks' src="/assets/img22.jpg" alt="" />
+        <img className='artworks' src="/assets/img23.jpg" alt="" />
+        <img className='artworks' src="/assets/img24.jpg" alt="" />
+        <img className='artworks' src="/assets/img25.jpg" alt="" />
+        <img className='artworks' src="/assets/img26.jpg" alt="" />
+        <img className='artworks' src="/assets/img27.jpg" alt="" />
+        <img className='artworks' src="/assets/img28.jpg" alt="" />
+        <img className='artworks' src="/assets/img30.jpg" alt="" />
+        <img className='artworks' src="/assets/img31.jpg" alt="" />
       </section>
       </div>
 
